a4e9871637659a153beaad11f9ef4fd2f4b8b78d
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-05-30 01:56:19 +0200

    added javascript tp ensure nav links works

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const targetElement = document.querySelector(this.getAttribute('href'));
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
